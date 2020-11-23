133b4b98d6b044c17f3b6fab566515579afe34e8
2214962083 <2214962083@qq.com>
2020-11-24 01:45:19 +0800

    调通测试用例

--- src/lib/base.ts
+++ src/lib/base.ts
@@ -110,140 +110,231 @@ class Base {
    * md5 加密字符串
    * @param {string} str 要加密成md5的数据
    */
-  md5(str: string): string {
-    function d(n: number, t: number) {
-      const r = (65535 & n) + (65535 & t)
-      return (((n >> 16) + (t >> 16) + (r >> 16)) << 16) | (65535 & r)
+  md5(string: string): string {
+    function RotateLeft(lValue: number, iShiftBits: number) {
+      return (lValue << iShiftBits) | (lValue >>> (32 - iShiftBits))
     }
-    function f(n: number, t: number, r: number, e: number, o: number, u: number) {
-      let c: number, f: number
-      return d(((c = d(d(t, n), d(e, u))) << (f = o)) | (c >>> (32 - f)), r)
-    }
-    function l(n: number, t: number, r: number, e: number, o: number, u: number, c: number) {
-      return f((t & r) | (~t & e), n, t, o, u, c)
-    }
-    function v(n: number, t: number, r: number, e: number, o: number, u: number, c: number) {
-      return f((t & e) | (r & ~e), n, t, o, u, c)
-    }
-    function g(n: number, t: number, r: number, e: number, o: number, u: number, c: number) {
-      return f(t ^ r ^ e, n, t, o, u, c)
-    }
-    function m(n: number, t: number, r: number, e: number, o: number, u: number, c: number) {
-      return f(r ^ (t | ~e), n, t, o, u, c)
-    }
-    function i(n: number[], t: number) {
-      let r, e, o, u
-      ;(n[t >> 5] |= 128 << t % 32), (n[14 + (((t + 64) >>> 9) << 4)] = t)
-      let c = 1732584193
-      for (let f = -271733879, i = -1732584194, a = 271733878, h = 0; h < n.length; h += 16)
-        (c = l((r = c), (e = f), (o = i), (u = a), n[h], 7, -680876936)),
-          (a = l(a, c, f, i, n[h + 1], 12, -389564586)),
-          (i = l(i, a, c, f, n[h + 2], 17, 606105819)),
-          (f = l(f, i, a, c, n[h + 3], 22, -1044525330)),
-          (c = l(c, f, i, a, n[h + 4], 7, -176418897)),
-          (a = l(a, c, f, i, n[h + 5], 12, 1200080426)),
-          (i = l(i, a, c, f, n[h + 6], 17, -1473231341)),
-          (f = l(f, i, a, c, n[h + 7], 22, -45705983)),
-          (c = l(c, f, i, a, n[h + 8], 7, 1770035416)),
-          (a = l(a, c, f, i, n[h + 9], 12, -1958414417)),
-          (i = l(i, a, c, f, n[h + 10], 17, -42063)),
-          (f = l(f, i, a, c, n[h + 11], 22, -1990404162)),
-          (c = l(c, f, i, a, n[h + 12], 7, 1804603682)),
-          (a = l(a, c, f, i, n[h + 13], 12, -40341101)),
-          (i = l(i, a, c, f, n[h + 14], 17, -1502002290)),
-          (c = v(c, (f = l(f, i, a, c, n[h + 15], 22, 1236535329)), i, a, n[h + 1], 5, -165796510)),
-          (a = v(a, c, f, i, n[h + 6], 9, -1069501632)),
-          (i = v(i, a, c, f, n[h + 11], 14, 643717713)),
-          (f = v(f, i, a, c, n[h], 20, -373897302)),
-          (c = v(c, f, i, a, n[h + 5], 5, -701558691)),
-          (a = v(a, c, f, i, n[h + 10], 9, 38016083)),
-          (i = v(i, a, c, f, n[h + 15], 14, -660478335)),
-          (f = v(f, i, a, c, n[h + 4], 20, -405537848)),
-          (c = v(c, f, i, a, n[h + 9], 5, 568446438)),
-          (a = v(a, c, f, i, n[h + 14], 9, -1019803690)),
-          (i = v(i, a, c, f, n[h + 3], 14, -187363961)),
-          (f = v(f, i, a, c, n[h + 8], 20, 1163531501)),
-          (c = v(c, f, i, a, n[h + 13], 5, -1444681467)),
-          (a = v(a, c, f, i, n[h + 2], 9, -51403784)),
-          (i = v(i, a, c, f, n[h + 7], 14, 1735328473)),
-          (c = g(c, (f = v(f, i, a, c, n[h + 12], 20, -1926607734)), i, a, n[h + 5], 4, -378558)),
-          (a = g(a, c, f, i, n[h + 8], 11, -2022574463)),
-          (i = g(i, a, c, f, n[h + 11], 16, 1839030562)),
-          (f = g(f, i, a, c, n[h + 14], 23, -35309556)),
-          (c = g(c, f, i, a, n[h + 1], 4, -1530992060)),
-          (a = g(a, c, f, i, n[h + 4], 11, 1272893353)),
-          (i = g(i, a, c, f, n[h + 7], 16, -155497632)),
-          (f = g(f, i, a, c, n[h + 10], 23, -1094730640)),
-          (c = g(c, f, i, a, n[h + 13], 4, 681279174)),
-          (a = g(a, c, f, i, n[h], 11, -358537222)),
-          (i = g(i, a, c, f, n[h + 3], 16, -722521979)),
-          (f = g(f, i, a, c, n[h + 6], 23, 76029189)),
-          (c = g(c, f, i, a, n[h + 9], 4, -640364487)),
-          (a = g(a, c, f, i, n[h + 12], 11, -421815835)),
-          (i = g(i, a, c, f, n[h + 15], 16, 530742520)),
-          (c = m(c, (f = g(f, i, a, c, n[h + 2], 23, -995338651)), i, a, n[h], 6, -198630844)),
-          (a = m(a, c, f, i, n[h + 7], 10, 1126891415)),
-          (i = m(i, a, c, f, n[h + 14], 15, -1416354905)),
-          (f = m(f, i, a, c, n[h + 5], 21, -57434055)),
-          (c = m(c, f, i, a, n[h + 12], 6, 1700485571)),
-          (a = m(a, c, f, i, n[h + 3], 10, -1894986606)),
-          (i = m(i, a, c, f, n[h + 10], 15, -1051523)),
-          (f = m(f, i, a, c, n[h + 1], 21, -2054922799)),
-          (c = m(c, f, i, a, n[h + 8], 6, 1873313359)),
-          (a = m(a, c, f, i, n[h + 15], 10, -30611744)),
-          (i = m(i, a, c, f, n[h + 6], 15, -1560198380)),
-          (f = m(f, i, a, c, n[h + 13], 21, 1309151649)),
-          (c = m(c, f, i, a, n[h + 4], 6, -145523070)),
-          (a = m(a, c, f, i, n[h + 11], 10, -1120210379)),
-          (i = m(i, a, c, f, n[h + 2], 15, 718787259)),
-          (f = m(f, i, a, c, n[h + 9], 21, -343485551)),
-          (c = d(c, r)),
-          (f = d(f, e)),
-          (i = d(i, o)),
-          (a = d(a, u))
-      return [c, f, i, a] as number[]
-    }
-    function a(n: number[]) {
-      let t = ''
-      for (let r = 32 * n.length, e = 0; e < r; e += 8) t += String.fromCharCode((n[e >> 5] >>> e % 32) & 255)
-      return t
-    }
-    function h(n: string) {
-      const t: (number | undefined)[] = []
-      let e
-      for (t[(n.length >> 2) - 1] = void 0, e = 0; e < t.length; e += 1) t[e] = 0
-      for (let r = 8 * n.length, e = 0; e < r; e += 8) (t[e >> 5] as number) |= (255 & n.charCodeAt(e / 8)) << e % 32
-      return t as number[]
-    }
-    function e(n: string) {
-      for (let t, r = '0123456789abcdef', e = '', o = 0; o < n.length; o += 1)
-        (t = n.charCodeAt(o)), (e += r.charAt((t >>> 4) & 15) + r.charAt(15 & t))
-      return e
-    }
-    function r(n: string) {
-      return unescape(encodeURIComponent(n))
-    }
-    function o(n: string) {
-      let t: string
-      return a(i(h((t = r(n))), 8 * t.length))
-    }
-    function u(n: string, t: string) {
-      return (function (n, t) {
-        let r,
-          e,
-          o = h(n)
 
-        const u: (number | undefined)[] = [],
-          c: (number | undefined)[] = []
-        for (u[15] = c[15] = void 0, 16 < o.length && (o = i(o, 8 * n.length)), r = 0; r < 16; r += 1)
-          (u[r] = 909522486 ^ o[r]), (c[r] = 1549556828 ^ o[r])
-        return (e = i((u as number[]).concat(h(t)), 512 + 8 * t.length)), a(i((c as number[]).concat(e), 640))
-      })(r(n), r(t))
+    function AddUnsigned(lX: number, lY: number) {
+      const lX8 = lX & 0x80000000
+      const lY8 = lY & 0x80000000
+      const lX4 = lX & 0x40000000
+      const lY4 = lY & 0x40000000
+      const lResult = (lX & 0x3fffffff) + (lY & 0x3fffffff)
+      if (lX4 & lY4) {
+        return lResult ^ 0x80000000 ^ lX8 ^ lY8
+      }
+      if (lX4 | lY4) {
+        if (lResult & 0x40000000) {
+          return lResult ^ 0xc0000000 ^ lX8 ^ lY8
+        } else {
+          return lResult ^ 0x40000000 ^ lX8 ^ lY8
+        }
+      } else {
+        return lResult ^ lX8 ^ lY8
+      }
     }
-    function t(n: string, t?: string, r?: number) {
-      return t ? (r ? u(t, n) : e(u(t, n))) : r ? o(n) : e(o(n))
+
+    function F(x: number, y: number, z: number) {
+      return (x & y) | (~x & z)
     }
-    return t(str) as string
+
+    function G(x: number, y: number, z: number) {
+      return (x & z) | (y & ~z)
+    }
+
+    function H(x: number, y: number, z: number) {
+      return x ^ y ^ z
+    }
+
+    function I(x: number, y: number, z: number) {
+      return y ^ (x | ~z)
+    }
+
+    function FF(a: number, b: number, c: number, d: number, x: number, s: number, ac: number) {
+      a = AddUnsigned(a, AddUnsigned(AddUnsigned(F(b, c, d), x), ac))
+      return AddUnsigned(RotateLeft(a, s), b)
+    }
+
+    function GG(a: number, b: number, c: number, d: number, x: number, s: number, ac: number) {
+      a = AddUnsigned(a, AddUnsigned(AddUnsigned(G(b, c, d), x), ac))
+      return AddUnsigned(RotateLeft(a, s), b)
+    }
+
+    function HH(a: number, b: number, c: number, d: number, x: number, s: number, ac: number) {
+      a = AddUnsigned(a, AddUnsigned(AddUnsigned(H(b, c, d), x), ac))
+      return AddUnsigned(RotateLeft(a, s), b)
+    }
+
+    function II(a: number, b: number, c: number, d: number, x: number, s: number, ac: number) {
+      a = AddUnsigned(a, AddUnsigned(AddUnsigned(I(b, c, d), x), ac))
+      return AddUnsigned(RotateLeft(a, s), b)
+    }
+
+    function ConvertToWordArray(string: string): number[] {
+      let lWordCount
+      const lMessageLength = string.length
+      const lNumberOfWords_temp1 = lMessageLength + 8
+      const lNumberOfWords_temp2 = (lNumberOfWords_temp1 - (lNumberOfWords_temp1 % 64)) / 64
+      const lNumberOfWords = (lNumberOfWords_temp2 + 1) * 16
+      const lWordArray = Array(lNumberOfWords - 1)
+      let lBytePosition = 0
+      let lByteCount = 0
+      while (lByteCount < lMessageLength) {
+        lWordCount = (lByteCount - (lByteCount % 4)) / 4
+        lBytePosition = (lByteCount % 4) * 8
+        lWordArray[lWordCount] = lWordArray[lWordCount] | (string.charCodeAt(lByteCount) << lBytePosition)
+        lByteCount++
+      }
+      lWordCount = (lByteCount - (lByteCount % 4)) / 4
+      lBytePosition = (lByteCount % 4) * 8
+      lWordArray[lWordCount] = lWordArray[lWordCount] | (0x80 << lBytePosition)
+      lWordArray[lNumberOfWords - 2] = lMessageLength << 3
+      lWordArray[lNumberOfWords - 1] = lMessageLength >>> 29
+      return lWordArray
+    }
+
+    function WordToHex(lValue: number) {
+      let WordToHexValue = '',
+        WordToHexValue_temp = '',
+        lByte,
+        lCount
+      for (lCount = 0; lCount <= 3; lCount++) {
+        lByte = (lValue >>> (lCount * 8)) & 255
+        WordToHexValue_temp = '0' + lByte.toString(16)
+        WordToHexValue = WordToHexValue + WordToHexValue_temp.substr(WordToHexValue_temp.length - 2, 2)
+      }
+      return WordToHexValue
+    }
+
+    function Utf8Encode(string: string): string {
+      string = string.replace(/\r\n/g, '\n')
+      let utftext = ''
+
+      for (let n = 0; n < string.length; n++) {
+        const c = string.charCodeAt(n)
+
+        if (c < 128) {
+          utftext += String.fromCharCode(c)
+        } else if (c > 127 && c < 2048) {
+          utftext += String.fromCharCode((c >> 6) | 192)
+          utftext += String.fromCharCode((c & 63) | 128)
+        } else {
+          utftext += String.fromCharCode((c >> 12) | 224)
+          utftext += String.fromCharCode(((c >> 6) & 63) | 128)
+          utftext += String.fromCharCode((c & 63) | 128)
+        }
+      }
+
+      return utftext
+    }
+
+    let x = []
+    let k, AA, BB, CC, DD, a, b, c, d
+    const S11 = 7,
+      S12 = 12,
+      S13 = 17,
+      S14 = 22
+    const S21 = 5,
+      S22 = 9,
+      S23 = 14,
+      S24 = 20
+    const S31 = 4,
+      S32 = 11,
+      S33 = 16,
+      S34 = 23
+    const S41 = 6,
+      S42 = 10,
+      S43 = 15,
+      S44 = 21
+
+    string = Utf8Encode(string)
+
+    x = ConvertToWordArray(string)
+
+    a = 0x67452301
+    b = 0xefcdab89
+    c = 0x98badcfe
+    d = 0x10325476
+
+    for (k = 0; k < x.length; k += 16) {
+      AA = a
+      BB = b
+      CC = c
+      DD = d
+      a = FF(a, b, c, d, x[k + 0], S11, 0xd76aa478)
+      d = FF(d, a, b, c, x[k + 1], S12, 0xe8c7b756)
+      c = FF(c, d, a, b, x[k + 2], S13, 0x242070db)
+      b = FF(b, c, d, a, x[k + 3], S14, 0xc1bdceee)
+      a = FF(a, b, c, d, x[k + 4], S11, 0xf57c0faf)
+      d = FF(d, a, b, c, x[k + 5], S12, 0x4787c62a)
+      c = FF(c, d, a, b, x[k + 6], S13, 0xa8304613)
+      b = FF(b, c, d, a, x[k + 7], S14, 0xfd469501)
+      a = FF(a, b, c, d, x[k + 8], S11, 0x698098d8)
+      d = FF(d, a, b, c, x[k + 9], S12, 0x8b44f7af)
+      c = FF(c, d, a, b, x[k + 10], S13, 0xffff5bb1)
+      b = FF(b, c, d, a, x[k + 11], S14, 0x895cd7be)
+      a = FF(a, b, c, d, x[k + 12], S11, 0x6b901122)
+      d = FF(d, a, b, c, x[k + 13], S12, 0xfd987193)
+      c = FF(c, d, a, b, x[k + 14], S13, 0xa679438e)
+      b = FF(b, c, d, a, x[k + 15], S14, 0x49b40821)
+      a = GG(a, b, c, d, x[k + 1], S21, 0xf61e2562)
+      d = GG(d, a, b, c, x[k + 6], S22, 0xc040b340)
+      c = GG(c, d, a, b, x[k + 11], S23, 0x265e5a51)
+      b = GG(b, c, d, a, x[k + 0], S24, 0xe9b6c7aa)
+      a = GG(a, b, c, d, x[k + 5], S21, 0xd62f105d)
+      d = GG(d, a, b, c, x[k + 10], S22, 0x2441453)
+      c = GG(c, d, a, b, x[k + 15], S23, 0xd8a1e681)
+      b = GG(b, c, d, a, x[k + 4], S24, 0xe7d3fbc8)
+      a = GG(a, b, c, d, x[k + 9], S21, 0x21e1cde6)
+      d = GG(d, a, b, c, x[k + 14], S22, 0xc33707d6)
+      c = GG(c, d, a, b, x[k + 3], S23, 0xf4d50d87)
+      b = GG(b, c, d, a, x[k + 8], S24, 0x455a14ed)
+      a = GG(a, b, c, d, x[k + 13], S21, 0xa9e3e905)
+      d = GG(d, a, b, c, x[k + 2], S22, 0xfcefa3f8)
+      c = GG(c, d, a, b, x[k + 7], S23, 0x676f02d9)
+      b = GG(b, c, d, a, x[k + 12], S24, 0x8d2a4c8a)
+      a = HH(a, b, c, d, x[k + 5], S31, 0xfffa3942)
+      d = HH(d, a, b, c, x[k + 8], S32, 0x8771f681)
+      c = HH(c, d, a, b, x[k + 11], S33, 0x6d9d6122)
+      b = HH(b, c, d, a, x[k + 14], S34, 0xfde5380c)
+      a = HH(a, b, c, d, x[k + 1], S31, 0xa4beea44)
+      d = HH(d, a, b, c, x[k + 4], S32, 0x4bdecfa9)
+      c = HH(c, d, a, b, x[k + 7], S33, 0xf6bb4b60)
+      b = HH(b, c, d, a, x[k + 10], S34, 0xbebfbc70)
+      a = HH(a, b, c, d, x[k + 13], S31, 0x289b7ec6)
+      d = HH(d, a, b, c, x[k + 0], S32, 0xeaa127fa)
+      c = HH(c, d, a, b, x[k + 3], S33, 0xd4ef3085)
+      b = HH(b, c, d, a, x[k + 6], S34, 0x4881d05)
+      a = HH(a, b, c, d, x[k + 9], S31, 0xd9d4d039)
+      d = HH(d, a, b, c, x[k + 12], S32, 0xe6db99e5)
+      c = HH(c, d, a, b, x[k + 15], S33, 0x1fa27cf8)
+      b = HH(b, c, d, a, x[k + 2], S34, 0xc4ac5665)
+      a = II(a, b, c, d, x[k + 0], S41, 0xf4292244)
+      d = II(d, a, b, c, x[k + 7], S42, 0x432aff97)
+      c = II(c, d, a, b, x[k + 14], S43, 0xab9423a7)
+      b = II(b, c, d, a, x[k + 5], S44, 0xfc93a039)
+      a = II(a, b, c, d, x[k + 12], S41, 0x655b59c3)
+      d = II(d, a, b, c, x[k + 3], S42, 0x8f0ccc92)
+      c = II(c, d, a, b, x[k + 10], S43, 0xffeff47d)
+      b = II(b, c, d, a, x[k + 1], S44, 0x85845dd1)
+      a = II(a, b, c, d, x[k + 8], S41, 0x6fa87e4f)
+      d = II(d, a, b, c, x[k + 15], S42, 0xfe2ce6e0)
+      c = II(c, d, a, b, x[k + 6], S43, 0xa3014314)
+      b = II(b, c, d, a, x[k + 13], S44, 0x4e0811a1)
+      a = II(a, b, c, d, x[k + 4], S41, 0xf7537e82)
+      d = II(d, a, b, c, x[k + 11], S42, 0xbd3af235)
+      c = II(c, d, a, b, x[k + 2], S43, 0x2ad7d2bb)
+      b = II(b, c, d, a, x[k + 9], S44, 0xeb86d391)
+      a = AddUnsigned(a, AA)
+      b = AddUnsigned(b, BB)
+      c = AddUnsigned(c, CC)
+      d = AddUnsigned(d, DD)
+    }
+
+    const temp = WordToHex(a) + WordToHex(b) + WordToHex(c) + WordToHex(d)
+
+    return temp.toLowerCase()
   }
 
   /**
